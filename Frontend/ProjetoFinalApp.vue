187b7a65a43412916028991ede02eb4145221d0b
André Carvalho <77457037+AndreLuisOak@users.noreply.github.com>
2023-05-16 20:18:43 -0300

    frontend do projeto final.

---
+++ Frontend/ProjetoFinalApp.vue
@@ -0,0 +1,42 @@
+<script setup lang="js">
+import { ref } from 'vue'
+
+</script>
+
+<template>
+    <div class="album py-5 bg-body-tertiary">
+      <div class="container">
+         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col" v-for="manga in mangas">
+              <MangaCard :title="manga.title" :price="manga.price" :number="manga.number"></MangaCard>
+            </div>
+              <div class="card shadow-sm">
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-body-secondary">9 mins</small>
+                  </div>
+                </div>
+              </div>
+  </template>
+  
+  <style scoped>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
+  </style>
+  
