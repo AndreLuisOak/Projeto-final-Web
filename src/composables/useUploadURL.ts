4313d472233a9f11844b835978b19612ce0d8ef0
André Carvalho <77457037+AndreLuisOak@users.noreply.github.com>
2023-06-20 15:25:03 -0300

    Delete src directory

--- src/composables/useUploadURL.ts
+++
@@ -1,7 +0,0 @@
-import { baseURL } from '../configPf';
-
-function getUploadURL(url: string) {
-    return `${baseURL}${url}`
-  }
-
-export const useUploadFile = (url: string) => getUploadURL(url)
--- src/configPf.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const baseURL = 'http://localhost:1337/admin/'
-
-export const api = axios.create({
-  baseURL: `${baseURL}/api/`,
-  timeout: 1000,
-});
